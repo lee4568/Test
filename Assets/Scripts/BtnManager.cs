0c1a3149e2510c8897d4aaca66a19b751d4e3369
lee4568 <dlfdlfgh@naver.com>
2018-04-30 17:16:54 +0900

    Initial commit

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnManager : MonoBehaviour {
+
+    public GameObject[] Btn;
+	
+	void SelectBtn()
+    {
+
+    }
+
+    void ExitBtn()
+    {
+
+    }
+
+}
