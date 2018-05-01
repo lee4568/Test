39a8a5ac8af0c1253754ddb2e5d1a450b0cd59ea
lee4568 <dlfdlfgh@naver.com>
2018-05-01 15:36:58 +0900

    캐릭터 이동
    Move

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public float speed = 10f;
+    public float rotspeed = 100f;
+
+	void Start ()
+    {
+		
+	}	
+
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * v * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * h * rotspeed * Time.deltaTime);
+    }
+}
