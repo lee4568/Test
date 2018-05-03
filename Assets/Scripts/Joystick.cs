572774fcb5c1b6fb05ddd2c63cfa5a9d5feaadb9
lee4568 <dlfdlfgh@naver.com>
2018-05-03 16:34:12 +0900

    조이스틱 테스트

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -1,74 +1,181 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Joystick : MonoBehaviour
-{
-    public Transform Player;       //플레이어
-    public Transform Stick;        //조이스틱
+public class JoyStick : MonoBehaviour {
 
-    private Vector3 StickFirstPos; //조이스틱의 처음 위치(가운데)
-    private Vector3 JoyVec;        //조이스틱의 벡터(방향)
-    private float Radius;          //조이스틱 배경의 반지름;
-    private bool MoveFlag;         //플레이어 움직임 스위치
-
-    public float movespeed;
-    public GameObject player;
-    
-    
-	
-	void Start ()
+    public Texture areaTexture;
+    public Texture touchTexture;
+    public Vector2 joystickPosition = new Vector2(135f, 135f);
+    public Vector2 speed = new Vector2(2, 100);
+    public float zoneRadius = 100f;
+    public float touchSize = 30;
+    public float deadZone = 20;
+    public float touchSizeCoef = 0;
+    protected Vector2 joystickAxis;
+    protected Vector2 joystickValue;
+    public Vector2 joyTouch;
+    private Vector2 _joystickCenter;
+    [SerializeField]
+    private Vector2 _smoothing = new Vector2(20f, 20f);
+    public Vector2 Smoothing
     {
-        Radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f;
-        StickFirstPos = Stick.transform.position;
-
-        //캔버스 크기에 대한 반지름 조절
-        float Can = transform.parent.GetComponent<RectTransform>().localScale.x;
-        Radius *= Can;
-
-        MoveFlag = false;
-	}
-	
-	//드래그
-    public void Drag(BaseEventData _Data)
-    {
-        PointerEventData Data = _Data as PointerEventData;
-        Vector3 Pos = Data.position;
-
-        //조이스틱을 이동시킬 방향을 구함
-        JoyVec = (Pos - StickFirstPos).normalized;
-
-        //조이스틱의 처음 위치와 내가 터치 하고있는 거리의 위치를 구한다
-        float Dis = Vector3.Distance(Pos, StickFirstPos);
-
-        //거리가 반지름보다 작으면 조이스틱을 현재 터치하고 있는곳으로 이동
-        if (Dis < Radius)
+        get
         {
-            Stick.position = StickFirstPos + JoyVec * Dis;
+            return this._smoothing;
         }
-        //거리가 반지름보다 커지면 조이스틱을 반지름의 크기만큼으로 이동(조이스틱이 원밖으로 나가지 않도록)
+        set
+        {
+            _smoothing = value;
+            if (_smoothing.x < 0.1f)
+            {
+                _smoothing.x = 0.1f;
+            }
+            if (_smoothing.y < 0.1)
+            {
+                _smoothing.y = 0.1f;
+            }
+        }
+    }
+    private int _joystickIndex = -1;
+    private bool _enaReset;
+    private bool _enaZoom;
+
+    void Start()
+    {
+        _joystickCenter = joystickPosition;
+        _enaReset = false;
+    }
+
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.Android)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    if (_joystickIndex == touch.fingerId)
+                    {
+                        _joystickIndex = -1;
+                        _enaReset = true;
+                    }
+                }
+
+                if (_joystickIndex == touch.fingerId)
+                {
+                    OnTouchDown(touch.position);
+                }
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (((Vector2)touch.position - _joystickCenter).sqrMagnitude < Mathf.Pow((zoneRadius + touchSizeCoef / 2), 2))
+                    {
+                        _joystickIndex = touch.fingerId;
+                    }
+                }
+            }
+
+            UpdateJoystick();
+            if (_enaReset)
+            {
+                ResetJoystick();
+            }
+        }
         else
-            Stick.position = StickFirstPos + JoyVec * Radius;
-
-        Player.eulerAngles = new Vector3(0, Mathf.Atan2(JoyVec.x, JoyVec.y) * Mathf.Rad2Deg, 0);
-
-        player.transform.Translate(0, 0, movespeed * Time.deltaTime); //플레이어 이동
-    }
-
-    //드래그 끝
-    public void DragEnd()
-    {
-        Stick.position = StickFirstPos;  //스틱을 원래 위치로 이동
-        JoyVec = Vector3.zero;           //방향을 0으로
-
-        MoveFlag = false;
-    }
-	void Update ()
-    {
-		if(MoveFlag)
         {
-            Player.transform.Translate(Vector3.forward * Time.deltaTime * 10f);
+            if (Input.GetButtonUp("Fire1"))
+            {
+                _joystickIndex = -1;
+                _enaReset = true;
+            }
+            if (_joystickIndex == 1)
+            {
+                OnTouchDown(Input.mousePosition);
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (((Vector2)Input.mousePosition - _joystickCenter).sqrMagnitude < Mathf.Pow((zoneRadius + touchSizeCoef / 2), 2))
+                {
+                    _joystickIndex = 1;
+
+                }
+
+            }
+            if (_enaReset)
+            {
+                ResetJoystick();
+            }
+
+            UpdateJoystick();
+
         }
-	}
+
+    }
+
+
+
+
+    private void UpdateJoystick()
+    {
+        if (joyTouch.sqrMagnitude > deadZone * deadZone)
+        {
+
+            joystickAxis = Vector2.zero;
+            if (Mathf.Abs(joyTouch.x) > deadZone)
+            {
+                joystickAxis = new Vector2((joyTouch.x - (deadZone * Mathf.Sign(joyTouch.x))) / (zoneRadius - touchSizeCoef - deadZone), joystickAxis.y);
+
+            }
+            else
+            {
+                joystickAxis = new Vector2(joyTouch.x / (zoneRadius - touchSizeCoef), joystickAxis.y);
+
+            }
+            if (Mathf.Abs(joyTouch.y) > deadZone)
+            {
+                joystickAxis = new Vector2(joystickAxis.x, (joyTouch.y - (deadZone * Mathf.Sign(joyTouch.y))) / (zoneRadius - touchSizeCoef - deadZone));
+            }
+            else
+            {
+                joystickAxis = new Vector2(joystickAxis.x, joyTouch.y / (zoneRadius - touchSizeCoef));
+            }
+
+        }
+        else
+        {
+            joystickAxis = new Vector2(0, 0);
+        }
+        Vector2 realvalue = new Vector2(speed.x * joystickAxis.x, speed.y * joystickAxis.y);
+        joystickValue = realvalue;
+        
+    }
+
+    void OnTouchDown(Vector2 position)
+    {
+        joyTouch = new Vector2(position.x, position.y) - _joystickCenter;
+        if ((joyTouch / (zoneRadius - touchSizeCoef)).sqrMagnitude > 1)
+        {
+            joyTouch.Normalize();
+            joyTouch *= zoneRadius - touchSizeCoef;
+        }
+        //print(joyTouch);
+    }
+
+
+    private void ResetJoystick()
+    {
+        if (joyTouch.sqrMagnitude > 0.1)
+        {
+            joyTouch = new Vector2(joyTouch.x - joyTouch.x * _smoothing.x * Time.deltaTime, joyTouch.y - joyTouch.y * _smoothing.y * Time.deltaTime);
+        }
+        else
+        {
+            joyTouch = Vector2.zero;
+            _enaReset = false;
+        }
+    }
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(_joystickCenter.x - zoneRadius, Screen.height - _joystickCenter.y - zoneRadius, zoneRadius * 2, zoneRadius * 2), areaTexture, ScaleMode.ScaleToFit, true);
+        GUI.DrawTexture(new Rect(_joystickCenter.x + (joyTouch.x - touchSize), Screen.height - _joystickCenter.y - (joyTouch.y + touchSize), touchSize * 2, touchSize * 2), touchTexture, ScaleMode.ScaleToFit, true);
+    }
 }
