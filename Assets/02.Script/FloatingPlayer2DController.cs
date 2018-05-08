fc9a81b3a32a7765b90a9b8eb6cbae375a4b4a8a
lee4568 <dlfdlfgh@naver.com>
2018-05-08 13:39:56 +0900

    적 추가

---
+++ Assets/02.Script/FloatingPlayer2DController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FloatingPlayer2DController : MonoBehaviour
+{
+    public float moveForce = 5, boostMultiplier = 2;
+    Rigidbody2D myBody;
+	
+	void Start ()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        Vector3 moveVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical"),0) * moveForce;
+        bool isBoosting = CrossPlatformInputManager.GetButton("Boost");
+        //Debug.Log(isBoosting ? boostMultiplier : 1);
+        myBody.AddForce(moveVec * (isBoosting ? boostMultiplier : 1));
+	}
+}
---
+++ Assets/Scripts/EnemyState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour {
+
+    Animation anim;
+	
+	void Start ()
+    {
+        anim = GetComponent<Animation>();
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+
+        }
+    }
+
+
+}
