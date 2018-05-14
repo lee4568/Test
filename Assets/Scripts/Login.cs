fd908ec261eff03fae2b58da0a331c306ce0e552
lee4568 <dlfdlfgh@naver.com>
2018-05-14 18:05:10 +0900

    로그인 용 서버 생성

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+using SimpleJSON;
+
+public class Login : MonoBehaviour {
+
+    public string LoginURL = "http://ehtls489.cafe24.com/rpgserver/select.php"; // 로그인용 URL
+    public string SigninURL = "http://ehtls489.cafe24.com/rpgserver/insert.php"; // 아이디 생성 URL
+
+    public GameObject loginBtn; // 로그인 버튼
+
+   public void LoginBtn()
+    {
+        StartCoroutine(LoginCoroutine());
+    }
+
+   IEnumerator LoginCoroutine()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("UserID", PlayerPrefs.GetInt("UserID"));
+        form.AddField("UserPass", SystemInfo.deviceUniqueIdentifier);
+        WWW www = new WWW(LoginURL);
+        yield return www;
+        Debug.Log(www.text);
+    }
+}
